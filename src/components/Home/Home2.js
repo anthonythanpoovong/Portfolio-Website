3ed62ff7fc1438ba99cb98fa9115c053850b7c18
Anthony Thanpoovong <thanpoovong84@gmail.com>
2024-07-23 15:10:11 -0400

    Updated Home Page

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -18,37 +18,27 @@ function Home2() {
               LET ME <span className="purple"> INTRODUCE </span> MYSELF
             </h1>
             <p className="home-about-body">
-              I fell in love with programming and I have at least learnt
-              something, I think… 🤷‍♂️
+              
               <br />
-              <br />I am fluent in classics like
+              <br />My name is Anthony, and I'm from South East and Eastern Asian, but I've lived in
               <i>
-                <b className="purple"> C++, Javascript and Go. </b>
+                <b className="purple"> Toronto, Ontario </b>
               </i>
+                   my whole life
               <br />
               <br />
-              My field of Interest's are building new &nbsp;
+              My field of Interest's are&nbsp;
               <i>
-                <b className="purple">Web Technologies and Products </b> and
+                <b className="purple">Software Engineering, Software Development </b> and
                 also in areas related to{" "}
                 <b className="purple">
-                  Blockchain.
+                  Security Engineering
                 </b>
               </i>
               <br />
               <br />
-              Whenever possible, I also apply my passion for developing products
-              with <b className="purple">Node.js</b> and
-              <i>
-                <b className="purple">
-                  {" "}
-                  Modern Javascript Library and Frameworks
-                </b>
-              </i>
-              &nbsp; like
-              <i>
-                <b className="purple"> React.js and Next.js</b>
-              </i>
+              I'm a 5th year Software Engineering student at <b className="purple">Toronto Metropolitan University. </b>
+              Looking for new graduate positions for May 2025.
             </p>
           </Col>
           <Col md={4} className="myAvtar">
